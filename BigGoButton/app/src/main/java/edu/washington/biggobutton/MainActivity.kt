e1261bc88efad173003f88b73cb48802c1aeaabd
lynnkn <lynnkn@uw.edu>
2022-01-21 00:08:18 -0800

    added button functionality

--- BigGoButton/app/src/main/java/edu/washington/biggobutton/MainActivity.kt
+++ BigGoButton/app/src/main/java/edu/washington/biggobutton/MainActivity.kt
@@ -2,10 +2,24 @@ package edu.washington.biggobutton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    // variables
+    lateinit var bigBtn : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // initialize count state
+        var count = 0
+        // get button and set event listener
+        bigBtn = findViewById<Button>(R.id.bigBtn)
+        bigBtn.setOnClickListener{
+            count++
+            val includeS = if (count === 1) "time" else "times"
+            bigBtn.setText(getString(R.string.btn_push_text, count, includeS))
+        }
     }
 }
